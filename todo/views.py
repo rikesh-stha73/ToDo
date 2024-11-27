6fb5a27c042d90b3d3ca551efa6d6421f0c94f05
Rikesh Shrestha <rikesh.stha73@gmail.com>
2024-11-27 12:33:20 +0545

    TODO Finished

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,54 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
+from django.contrib import messages 
 
 # Create your views here.
 
 def addTask(request):
-    task = request.POST['task']
-    Task.objects.create(task=task)
+    if request.method == "POST":
+        task = request.POST.get('task')
+        if not task:  # Check if task is empty
+            messages.error(request, "Task cannot be empty!")  # Display error message to user
+            return redirect('home')
+        Task.objects.create(task=task)
+        return redirect('home')
+    
+def mark_as_done(request, pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    
     return redirect('home')
+
+def mark_as_undone(request, pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task=get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        new_task=request.POST.get('task')
+        if not new_task:  # Check if task is empty
+            messages.error(request, "Task cannot be empty!")  # Display error message to user
+            context = {
+                'get_task': get_task,
+            }
+            return render(request, 'edit_task.html', context)
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html',context)
+    
+
+def delete_task(request, pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
--- todo_main/views.py
+++ todo_main/views.py
@@ -5,7 +5,11 @@ from todo.models import Task
 
 def home(request):
     task = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    
+    completed_task = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    
     context = {
-        'tasks': task
+        'tasks': task,
+        'completed_task': completed_task,
     }
     return render(request, 'home.html', context)
