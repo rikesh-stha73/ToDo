24cc368727a883f46af240ad21299a29e82b5c5b
Rikesh Shrestha <rikesh.stha73@gmail.com>
2024-11-27 11:39:19 +0545

    new updates

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from todo.models import Task
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the todo_main home.")
+    task = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context = {
+        'tasks': task
+    }
+    return render(request, 'home.html', context)
